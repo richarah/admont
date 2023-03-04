9834e756eeabefab7853af55c0f4aa1911627f4e
Richard Alexander Haydon <richarah@stud.ntnu.no>
2023-03-04 01:54:59 +0100

    Updated run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from esr.esr import rank_sentences
 from model import *
 
-
+if __name__ == "__main__":
+    query = input("Query: ")
+    file_path = "test.txt"
+    sentences = extract_sentences(from_file(file_path))
+    result = format_sentences(rank_sentences(sentences, query)[:10])
+    for sent in result:
+        print(sent.strip())
+    # WIP: BLOOM integration
