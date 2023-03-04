d4ee1a2bf0eaba3843047cfb14033d6a3dc761be
Richard Alexander Haydon <richarah@stud.ntnu.no>
2023-03-04 01:56:12 +0100

    File dialog

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
+from tkinter import filedialog
 from esr.esr import rank_sentences
 from model import *
 
 if __name__ == "__main__":
     query = input("Query: ")
-    file_path = "test.txt"
+    file_path = filedialog.askopenfilename()
     sentences = extract_sentences(from_file(file_path))
     result = format_sentences(rank_sentences(sentences, query)[:10])
     for sent in result:
         print(sent.strip())
     # WIP: BLOOM integration
+
