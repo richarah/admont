b8310a7e3618310241b726e6bb1d47416dab5f94
Richard Alexander Haydon <richarah@stud.ntnu.no>
2023-03-04 02:54:26 +0100

    Fixed edge case preventing exit

--- run.py
+++ run.py
@@ -4,13 +4,22 @@ from tkinter import filedialog
 from esr.esr import *
 from model.model import *
 
+def extract_question(text):
+    question_index = text.find("Question:")
+    if question_index != -1:
+        question = "Question" + text[question_index + len("Question:"):].strip()
+        return question
+    else:
+        return text
+
+
 def ask_question(file_path, question, topic_sentences, hf_api_key, hf_model_id):
     sentences = extract_sentences(from_file(file_path))
     topic_sentences = format_sentences(rank_sentences(sentences, question)[:10])
     context = "\n".join(topic_sentences) + "\nQuestion: " + question + "\nAnswer: "
     response = query(context, hf_api_key, hf_model_id)
     os.system('clear')
-    print(response)
+    print(extract_question(response))
     return topic_sentences, response
 
 if __name__ == "__main__":
@@ -19,6 +28,8 @@ if __name__ == "__main__":
     hf_model_id = os.environ["HF_MODEL_ID"]
     file_path = filedialog.askopenfilename()
     question = input("Query: ")
+    if question == "exit":
+        break
     topic_sentences, response = ask_question(file_path, question, [], hf_api_key, hf_model_id)
     while True:
         text = input('"q" for new question, "a" to continue current answer, "exit" to exit: ').lower()
@@ -26,7 +37,9 @@ if __name__ == "__main__":
             break
         elif text == "q":
             question = input("Query: ")
+            if question == "exit":
+                break
             topic_sentences, response = ask_question(file_path, question, topic_sentences, hf_api_key, hf_model_id)
         elif text == "a":
             response = query(response, hf_api_key, hf_model_id)
-            print(response)
+            print(extract_question(response))
